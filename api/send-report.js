b4614485babf41f9cf8b8fb81f8b7d9109b1a11a
petrxkolar <petrxkolar@seznam.cz>
2026-05-04 15:45:08 +0200

    Update send-report.js

--- api/send-report.js
+++ api/send-report.js
@@ -8,7 +8,8 @@ export default async function handler(req, res) {
   try {
     const data = await resend.emails.send({
       from: 'Bezpečnostní Analýza <analyza@vasedomena.cz>', // Musí být vaše ověřená doména
-      to: [email], // Dynamický e-mail klienta z frontendu
+       to: <petrxkolar@seznam.cz>,
+      //to: [email], // Dynamický e-mail klienta z frontendu
       subject: `Security Report - ${name}`,
       html: `<p>Dobrý den, v příloze naleznete svůj report.</p>`,
       attachments: [
